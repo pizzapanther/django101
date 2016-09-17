9ced5ff1e8b33740f0af34af849abb80436509f3
Paul Bailey <paul@neutrondrive.com>
2016-09-17 20:26:56 +0000

    step13

--- collection/views.py
+++ collection/views.py
@@ -3,8 +3,14 @@ from django.template.response import TemplateResponse
 from collection.models import Thing
 
 def homepage (request):
-    things = Thing.objects.all()
+    search = request.GET.get('search', '')
     
+    if search:
+        things = Thing.objects.filter(name__icontains=search).order_by('name')
+        
+    else:
+        things = Thing.objects.all().order_by('name')
+        
     context = {
         'things': things
     }
