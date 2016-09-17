d61c2afeeacd60c00444ea4f3566a85d91996cf9
Paul Bailey <paul@neutrondrive.com>
2016-09-17 20:04:11 +0000

    step11

--- collection/admin.py
+++ collection/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from collection.models import Thing
 
-admin.site.register(Thing)
+@admin.register(Thing)
+class ThingAdmin (admin.ModelAdmin):
+    list_display = ('name', 'slug', 'Description')
+    prepopulated_fields = {'slug': ('name',)}
+    
+    def Description (self, obj):
+        return '<pre>{}</pre>'.format(obj.description)
+        
+    Description.allow_tags = True
+    
